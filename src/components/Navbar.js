4bf0958ee53dac322789f8a88ccb2f9c6e40a1f3
forrestmorrison <forrestmorrison@gmail.com>
2023-07-20 15:55:32 -0500

    added links with basic styling to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,34 @@
 import Logo from "../assets/4rest-logo-color.png"
+import { FaBars, FaTimes } from "react-icons/fa"
 
 const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-200'>
-        <div>
-          <img src={Logo} alt='logo' style={{ height: '50px' }}/>
-        </div>
+      <div>
+        <img src={Logo} alt='logo' style={{ height: '50px' }}/>
+      </div>
+      {/* menu */}
+      <div>
+        <ul className="flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      {/* hamburger */}
+      <div className="hidden">
+        <FaBars />
+      </div>
+      {/* mobile menu */}
+      <ul className="hidden">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
     </div>
   )
 }
