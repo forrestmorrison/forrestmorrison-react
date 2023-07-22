8e3f5233a02a39e1dd9d6832828b64149397aa8d
forrestmorrison <forrestmorrison@gmail.com>
2023-07-21 20:37:28 -0500

    added Forrest Name img to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HiArrowNarrowRight } from "react-icons/hi"
+import ForrestName from "../assets/forrest-name-green.png"
 
 const Home = () => {
   return (
@@ -7,9 +8,9 @@ const Home = () => {
         
         {/* Container */}
         <div className="max-w-[700px] mx-auto p-8 flex flex-col justify-center h-full">
-            <p className="font-bold sm:ml-1">Welcome to the official website of</p>
-            <h1 className="text-4xl sm:text-7xl font-bold text-[#119f05]">Forrest Morrison</h1>
-            <h2 className="py-4 text-2xl sm:text-4xl font-bold">Full-Stack Developer</h2>
+            <p className="pb-2">Welcome to the official website of</p>
+            <img src={ForrestName} alt="Forrest name" />
+            <h2 className="py-4 text-2xl sm:text-4xl">Full-Stack Developer</h2>
             <div>
                 <button className="px-6 py-3 my-2 flex items-center group text-white bg-[#119f05] hover:bg-[#5dbb10] duration-300">
                     View Work
---
+++ src/components/Work.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Work = () => {
+  return (
+    <div name="work" className="w-full md:h-screen">
+
+    </div>
+  )
+}
+
+export default Work
