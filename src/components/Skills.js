6dac2eb6561acdf9e6b8fe15065c296f5bfb701b
forrestmorrison <forrestmorrison@gmail.com>
2023-07-21 22:42:32 -0500

    skill items styling

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -25,67 +25,67 @@ const Skills = () => {
                 <p className="py-4">These are the technologies I've worked with:</p>
             </div>
             <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={HTML} alt="HTML icon" />
                     <p className="mb-4">HTML</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={CSS} alt="CSS icon" />
                     <p className="mb-4">CSS</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={Javascript} alt="JS icon" />
                     <p className="mb-4">Javascript</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={ReactImg} alt="React icon" />
                     <p className="mb-4">React</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={Git} alt="Git icon" />
                     <p className="mb-4">Git</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={Bootstrap} alt="Bootstrap icon" />
                     <p className="mb-4">Bootstrap</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={Tailwind} alt="Tailwind icon" />
                     <p className="mb-4">Tailwind CSS</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={MUI} alt="MUI icon" />
                     <p className="mb-4">Material UI</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={NodeJS} alt="Node icon" />
                     <p className="mb-4">Node JS</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={ExpressJS} alt="Express icon" />
                     <p className="mb-4">Express JS</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={MySQL} alt="MySQL icon" />
                     <p className="mb-4">MySQL</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={GoogleCloud} alt="Google Cloud icon" />
                     <p className="mb-4">Google Cloud</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={AWS} alt="AWS icon" />
                     <p className="mb-4">AWS</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={MongoDB} alt="MongoDB icon" />
                     <p className="mb-4">MongoDB</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={Firebase} alt="Firebase icon" />
                     <p className="mb-4">Firebase</p>
                 </div>
-                <div className="bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
+                <div className="rounded-md bg-gray-100 shadow-md shadow-gray-300 hover:scale-110 duration-500">
                     <img className="w-20 mx-auto py-4" src={Auth0} alt="Auth0 icon" />
                     <p className="mb-4">Auth0</p>
                 </div>
