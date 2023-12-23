60f2ec6130cb1695b5016c164dfb4ac6fe9a73cf
forrestmorrison <forrestmorrison@gmail.com>
2023-12-22 23:18:56 -0600

    FIXED DIVS OVERLAPPING NAVBAR ON HOVER BY ADDING Z INDEX

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav)
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-white shadow-sm'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-white shadow-sm z-10'>
       <div>
         <Link to="home" smooth={true} duration={500}>
           <a href="/">
