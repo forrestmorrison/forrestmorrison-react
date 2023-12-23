551e5df0326e5358f8bff9b1503a42224a771535
forrestmorrison <forrestmorrison@gmail.com>
2023-12-22 22:29:15 -0600

    Edited Home section animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,16 +8,34 @@ const Home = () => {
     <div name="home" className="w-full h-screen">
         
         {/* Container */}
-        <motion.div 
-            className="max-w-[900px] mx-auto p-8 flex flex-col justify-center h-full"
-            initial={{ scale: 0.8, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            transition={{ duration: 1 }}
-        >
-            <p className="pb-2">Welcome to the official website of</p>
-            <img src={ForrestName} alt="Forrest name" />
-            <h2 className="py-4 text-2xl sm:text-4xl">Full-Stack Developer</h2>
-            <div>
+        <div className="max-w-[900px] mx-auto p-8 flex flex-col justify-center h-full">
+            <motion.div
+                initial={{ scale: 0.8, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{ duration: 1, delay: 0.2 }}
+            >
+                <p className="pb-2">Welcome to the official website of</p>
+            </motion.div>
+            <motion.div
+                initial={{ scale: 0.8, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{ duration: 1 }}
+            >
+                <img src={ForrestName} alt="Forrest name" />
+            </motion.div>
+            <motion.div
+                initial={{ scale: 0.8, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{ duration: 1, delay: 0.3 }}
+            >
+                <h2 className="py-4 text-2xl sm:text-4xl">Full-Stack Developer</h2>
+            </motion.div>
+            
+            <motion.div
+                initial={{ scale: 0.95, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{ duration: 1, delay: 1 }}
+            >
                 <Link to="work" smooth={true} duration={500} offset={-70}>
                     <button className="px-6 py-3 my-2 flex items-center group text-white bg-[#119f05] hover:bg-[#5dbb10] duration-300">
                         View Work
@@ -26,8 +44,8 @@ const Home = () => {
                         </span>
                     </button>
                 </Link>
-            </div>
-        </motion.div>
+            </motion.div>
+        </div>
 
     </div>
   )
